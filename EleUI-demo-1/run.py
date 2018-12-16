b03cca666b808c75fc167ccc1b9073e189c8c516
1vec <29864621+1vec@users.noreply.github.com>
2018-12-16 16:00:08 +0800

    Finish jinja with home.html but not box-office.html

---
+++ EleUI-demo-1/run.py
@@ -0,0 +1,20 @@
+from flask import Flask, Response, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/box-office')
+def box():
+    return render_template('box-office.html')
+
+@app.route('/resource', methods=['POST'])
+def hello():
+    print(request.get_json())
+    data = count_type()
+    return Response(json.dumps(data))
+
+if __name__ == '__main__':
+    app.run()
