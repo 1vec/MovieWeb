b822b0285d9d68c0ea9c818accc4f4014d94ec8f
1vec <29864621+1vec@users.noreply.github.com>
2018-12-30 12:34:41 +0800

    使用python重写了启动命令

--- src/run.bat
+++
@@ -1,4 +0,0 @@
-cd /d %~dp0
-set FLASK_APP=movienest
-set FLASK_ENV=development
-flask run
--- src/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-echo "Enter dir $(pwd)"
-export FLASK_APP=movienest
-export FLASK_ENV=development
-flask run
